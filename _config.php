d6242f7f095c4e51b382821c7ed23e66363eca24
Ed Linklater <ed@silverstripe.com>
2015-11-01 01:52:12 +1300

    Show video information when valid YouTube ID found Pulls details from YouTube Data API v3, requires API key to be supplied via Configuration system

--- _config.php
+++ _config.php
@@ -1,4 +1,6 @@
 <?php
 
+define('SS_YOUTUBEFIELD_DIRECTORY', basename(dirname(__FILE__)));
+
 // Add custom style into the CMS. Done here instead of YAML so we can use dynamic module path and support SS 3.0.
-LeftAndMain::require_css(basename(dirname(__FILE__)) . '/css/LeftAndMainYouTubeField.css');
+LeftAndMain::require_css(SS_YOUTUBEFIELD_DIRECTORY . '/css/LeftAndMainYouTubeField.css');
