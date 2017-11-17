c1dd909da03a5f3d47971898c12c127a685aa164
priyashantha <priyashantha@silverstripers.com>
2017-11-17 14:25:15 +0530

    namespacing and basic ss4 upgrade

--- _config.php
+++ _config.php
@@ -1,6 +1,1 @@
 <?php
-
-define('SS_YOUTUBEFIELD_DIRECTORY', basename(dirname(__FILE__)));
-
-// Add custom style into the CMS. Done here instead of YAML so we can use dynamic module path and support SS 3.0.
-LeftAndMain::require_css(SS_YOUTUBEFIELD_DIRECTORY . '/css/LeftAndMainYouTubeField.css');
